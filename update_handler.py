65da88627765c430ee7b1a07fd959921063fe558
Rico <r.jambor@yahoo.de>
2016-03-04 15:46:30 +0100

    Fixed a bug with new players. Comments can be answered by the admin with the Telegram answer function.

--- update_handler.py
+++ update_handler.py
@@ -1,14 +1,16 @@
 __author__ = 'Rico'
+import datetime
+import time
+import re
+
 from messageSender import sendmessage
 from sql_handler import check_if_user_saved, sql_write
-import datetime, time
 
 
 def getUpdates(offset, bot):
     updates = bot.get_updates(offset+1).wait()
     returnlist = [[]]*0
 
-    #try:
     if bool(updates) and updates :
         print("\n--- Processing updates: ---")
         for update in updates:
@@ -21,12 +23,21 @@ def getUpdates(offset, bot):
                     username = update.message.sender.username
                     chat_id = update.message.chat.id
                     message_id = update.message.message_id
+                    reply_message_text = ""
+
+                    if update.message.reply_to_message is not None:
+                        reply_message_text = update.message.reply_to_message.text
+
+                        if reply_message_text is not None:
+                            try:
+                                pattern = re.compile("([0-9]{8,}) \|")
+                                reply_message_text = pattern.search(reply_message_text).group(1)
+                            except:
+                                reply_message_text = ""
 
                     if username is None:
-                        print("Username is None")
                         username = ""
                     if last_name is None:
-                        print("last_name is None")
                         last_name = ""
                     update_id = update.update_id
                     text = update.message.text
@@ -35,16 +46,15 @@ def getUpdates(offset, bot):
                     if user_index == -1:  # and chat_id == user_id:
                         print("New User")
                         sql_write(user_id, "en", first_name, last_name, username)  # neuen Nutzer hinzufügen
-                        text = ""
 
                     if chat_id > 0:
                         chat_type = "0"
                     else:
                         chat_type = "1"
 
-                    st = datetime.datetime.fromtimestamp(time.time()).strftime('%Y-%m-%d %H:%M:%S')
+                    st = datetime.datetime.fromtimestamp(time.time()).strftime('%Y-%m-%d %H:%M:%S')  # TODO print the time, the message was received by the server
                     print(st + "  -  Vorname: " + str(first_name) + " | Nachname: " + str(last_name) + " | Username: " + str(username) + " | UserID: " + str(user_id) + " | ChatType: " + str(chat_type) + " | Nachricht: " + str(text))
-                    templist.extend((user_id, update_id, first_name, last_name, text, chat_type, chat_id, message_id, username))
+                    templist.extend((user_id, update_id, first_name, last_name, text, chat_type, chat_id, message_id, username, reply_message_text))
                     returnlist.append(templist)
                 except AttributeError:
                     print("AttributeError ist aufgetreten!")
