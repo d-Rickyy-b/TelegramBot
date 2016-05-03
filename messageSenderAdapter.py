3e9f75ce8a360be02d69a3aca9a33e95d63c4208
Rico <r.jambor@yahoo.de>
2016-05-03 03:03:57 +0200

    MessageSenderAdapter is the new way to send messages. Not optimized and will likely change in the future.

--- messageSenderAdapter.py
+++ messageSenderAdapter.py
@@ -2,33 +2,44 @@ __author__ = 'Rico'
 from twx.botapi import ReplyKeyboardMarkup, ReplyKeyboardHide, ForceReply
 
 
-class messageSenderAdapter(object):
-    def sendmessage(self, chat_id, message_text, bot, message_id=None, keyboard=None, one_time_keyboard=None, force_reply=None):
-        if force_reply is not None:
-            bot.send_message(chat_id, message_text, reply_to_message_id=message_id, reply_markup=ForceReply.create(selective=True), parse_mode="Markdown").wait()
-        if keyboard is not None:
-            reply_markup = ReplyKeyboardMarkup.create(keyboard, selective=True, one_time_keyboard=one_time_keyboard)
-            bot.send_message(chat_id, message_text, reply_to_message_id=message_id, reply_markup=reply_markup, parse_mode="Markdown").wait()
+class MessageSenderAdapter(object):
+    def __sendmessage(self): #, chat_id, message_text, message_id=None, keyboard=None, one_time_keyboard=None, force_reply=None
+        if self.force_reply is not None:
+            self.bot.send_message(self.chat_id, self.text, reply_to_message_id=self.message_id, reply_markup=ForceReply.create(selective=True), parse_mode=self.parse_mode).wait()
+        elif self.keyboard is not None:
+            reply_markup = ReplyKeyboardMarkup.create(self.keyboard, selective=True, one_time_keyboard=self.one_time_keyboard)
+            self.bot.send_message(self.chat_id, self.text, reply_to_message_id=self.message_id, reply_markup=reply_markup, parse_mode=self.parse_mode).wait()
         else:
-            bot.send_message(chat_id, message_text, reply_to_message_id=message_id, parse_mode="Markdown").wait()
+            self.bot.send_message(self.chat_id, self.text, reply_to_message_id=self.message_id, parse_mode=self.parse_mode).wait()
         self.clear_message()
 
-    def send_joined_message(self, ):
-        self.sendmessage(self.chat_id, self.text, self.bot, self.message_id, self.keyboard, self.one_time_keyboard)
+    def send_new_message(self, chat_id, message_text, message_id=None, keyboard=None, one_time_keyboard=None, force_reply=None, parse_mode=None):
+        self.text = message_text
+        self.chat_id = chat_id
+        self.message_id = message_id #None?
+        self.keyboard = keyboard
+        self.one_time_keyboard = one_time_keyboard
+        self.force_reply = force_reply
+        self.parse_mode = parse_mode
+        self.__sendmessage()
+
+    def send_joined_message(self):
+        self.__sendmessage()
 
     def add_to_message(self, text):
         self.text += text
 
-    def set_metadata(self, message_id=None, keyboard=None, one_time_keyboard=None):
-        self.message_id = message_id
-        self.keyboard = keyboard
-        self.one_time_keyboard = one_time_keyboard
+    def set_metadata(self, message_id=None, keyboard=None, one_time_keyboard=None, parse_mode=None):
+        if not message_id is None: self.message_id = message_id
+        if not keyboard is None: self.keyboard = keyboard
+        if not one_time_keyboard is None:self.one_time_keyboard = one_time_keyboard
+        if not parse_mode is None:self.parse_mode = parse_mode
 
-    def hide_keyboard(self, chat_id, bot, text=None):
+    def hide_keyboard(self, chat_id, text=None):
         reply_markup = ReplyKeyboardHide.create()
         if text is None:
-            text = "👍"
-        bot.send_message(chat_id, text, reply_markup=reply_markup).wait()
+            text = "👍"#Thumbs up emoji
+        self.bot.send_message(chat_id, text, reply_markup=reply_markup).wait()
         self.clear_message()
 
     def clear_message(self):
@@ -41,6 +52,9 @@ class messageSenderAdapter(object):
         self.bot = bot
         self.text = ""
         self.chat_id = chat_id
-        self.message_id = 0
+        self.message_id = 0 #None?
         self.keyboard = None
         self.one_time_keyboard = None
+        self.force_reply = None
+        self.parse_mode = None
+
