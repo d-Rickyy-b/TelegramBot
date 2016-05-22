a55853c6e083af36aea11fe870d9cc2136140454
Rico <r.jambor@yahoo.de>
2016-05-22 23:47:36 +0200

    CamelCase'd classname

--- cardDeck.py
+++ cardDeck.py
@@ -2,7 +2,8 @@ __author__ = 'Rico'
 
 from language import translation
 
-class cardDeck:
+
+class CardDeck:
     symbols = ["♥", "♦", "♣", "♠"]
     valueInt = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
 
